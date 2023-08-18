d523dbf6ca26fef5764b5059b74cec7818a3612c
Aswathy Radha <aswathy.radha@gmail.com>
2023-08-18 16:28:49 -0500

    Adding a user defined function on classification metrics

---
+++ evaluate.py
@@ -0,0 +1,50 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.7
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+from sklearn.metrics import confusion_matrix
+
+def print_classification_metrics(actuals, predictions):
+    TN, FP, FN, TP = confusion_matrix(actuals, predictions).ravel()
+    ALL = TP + TN + FP + FN
+    
+    accuracy = (TP + TN)/ALL
+    print(f"Accuracy: {accuracy}")
+
+    true_positive_rate = TP/(TP+FN)
+    print(f"True Positive Rate: {true_positive_rate}")
+
+    false_positive_rate = FP/(FP+TN)
+    print(f"False Positive Rate: {false_positive_rate}")
+
+    true_negative_rate = TN/(TN+FP)
+    print(f"True Negative Rate: {true_negative_rate}")
+
+    false_negative_rate = FN/(FN+TP)
+    print(f"False Negative Rate: {false_negative_rate}")
+
+    precision = TP/(TP+FP)
+    print(f"Precision: {precision}")
+
+    recall = TP/(TP+FN)
+    print(f"Recall: {recall}")
+
+    f1_score = 2*(precision*recall)/(precision+recall)
+    print(f"F1 Score: {f1_score}")
+
+    support_pos = TP + FN
+    print(f"Support (0): {support_pos}")
+
+    support_neg = FP + TN
+    print(f"Support (1): {support_neg}")
